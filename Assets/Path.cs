0164a263a4f5a28d20246212884df41b799f61d9
yamisf000 <90745244+yamisf000@users.noreply.github.com>
2022-10-30 18:50:59 +0700

    placing tower

--- Assets/Path.cs
+++ Assets/Path.cs
@@ -6,6 +6,7 @@ public class Path : MonoBehaviour
 {
     [SerializeField]
     private Transform[] waypoints;
+    
 
     // Walk speed that can be set in Inspector
     [SerializeField]
@@ -14,6 +15,7 @@ public class Path : MonoBehaviour
     // Index of current waypoint from which Enemy walks
     // to the next one
     private int waypointIndex = 0;
+
 
     // Use this for initialization
     private void Start()
@@ -31,7 +33,29 @@ public class Path : MonoBehaviour
         Move();
     }
 
-    // Method that actually make Enemy walk
+/*    private void MoveUp()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (waypointUpIndex <= waypointsUP.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypointsUP[waypointUpIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == waypointsUP[waypointUpIndex].transform.position)
+            {
+                waypointUpIndex += 1;
+            }
+        }
+    }*/
+
     private void Move()
     {
         // If Enemy didn't reach last waypoint it can move
